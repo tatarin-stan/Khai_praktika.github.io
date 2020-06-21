3ccca4b29d09e97034bacfe753f0bc09516bacd8
tatarin-stan <tolikvasulyk@ukr.net>
2020-06-22 00:37:11 +0300

    123

---
+++ Практика/chart.js
@@ -0,0 +1,34 @@
+// ВЫБОР CHAR ЕЛЕМЕНТА
+const chart = document.querySelector(".chart");
+
+// СОЗДАНИЕ CANVAS ЕЛЕМЕНТА
+const canvas = document.createElement("canvas");
+canvas.width = 50;
+canvas.height = 50;
+
+// ХОЛС
+chart.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
+// ШИРИНА ЛИНИИ
+ctx.lineWidth = 8;
+
+// РАДИУС КОЛА
+const R = 20;
+
+function drawCircle(color, ratio, anticlockwise){
+
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.arc( canvas.width/2, canvas.height/2, R, 0, ratio * 2 * Math.PI, anticlockwise);
+    ctx.stroke();
+}
+
+function updateChart( income, outcome){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let ratio = income / (income+outcome);
+
+    drawCircle("#FFFFFF", - ratio, true);
+    drawCircle("#F0624D", 1 - ratio, false);
+}
